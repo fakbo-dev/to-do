27e537119bebd303746638b7f0d26fa1d2a29861
fakb-dev <fakbodev@gmail.com>
2025-04-03 20:09:00 -0400

    still reading Docs

--- src/main.js
+++ src/main.js
@@ -1,1 +1,2 @@
 const readingDocs = 'Im currently readin docs';
+const StillReadingDocs = 'I think im gonna have a week only reading docs after getting started because i have like 5 months of doing nothing about coding just sequel and database';
