162eb1775f73e74b6a4ccbf948b659faa08e3e17
fakb-dev <fakbodev@gmail.com>
2025-04-04 21:06:08 -0400

    n-word

--- src/main.js
+++ src/main.js
@@ -1,2 +1,3 @@
 const readingDocs = 'Im currently readin docs';
 const StillReadingDocs = 'I think im gonna have a week only reading docs after getting started because i have like 5 months of doing nothing about coding just sequel and database';
+const grappingSomeStuf = 'N word you know why';
